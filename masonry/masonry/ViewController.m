3b79694b2403bc35970036752de03f9615161619
wangchuang <191636716@qq.com>
2018-06-29 15:33:46 +0800

    firsr commit for it

---
+++ masonry/masonry/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  masonry
+//
+//  Created by Pro on 2018/6/29.
+//  Copyright © 2018年 Pro. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ masonry/masonry/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  masonry
+//
+//  Created by Pro on 2018/6/29.
+//  Copyright © 2018年 Pro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
